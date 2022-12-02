bc85fc5c4ce4b5d0894f7a412bb85c7f738f84c1
Michał Jarek <miciu77@gmail.com>
2022-12-02 19:21:30 +0100

    First conception

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from './Section/Section';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          fontSize: 40,
+          color: '#010101',
+          marginLeft: 20,
+        }}
+      >
+        <Section title='Phonebook'></Section>
+
+        <Section title='Contacts'></Section>
+      </div>
+    );
+  }
+}
+
+export { App };
