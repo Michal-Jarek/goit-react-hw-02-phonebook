a36ca54d0d8959ead1ad4edbbad0d2e5c7bec755
Michał Jarek <miciu77@gmail.com>
2022-12-06 17:36:19 +0100

    i found the BUG :D

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,32 @@ class App extends Component {
     contacts: [],
     name: '',
   };
+  // ********************* Methods **************
+
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(`Signed up as: ${this.state.name}`);
+    this.reset();
+  };
+
+  reset = () => {
+    const contactCopy = [...this.state.contacts];
+    contactCopy.push(this.state.name);
+    console.log(contactCopy);
+    this.setState({
+      contacts: contactCopy,
+      name: '',
+    });
+    console.log(this.state.name);
+  };
 
   render() {
+    const { name } = this.state;
+
     return (
       <div
         style={{
@@ -21,9 +45,25 @@ class App extends Component {
           marginLeft: 20,
         }}
       >
-        <Section title='Phonebook'></Section>
+        <Section title="Phonebook">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                onChange={this.handleChange}
+                value={name}
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </Section>
 
-        <Section title='Contacts'></Section>
+        <Section title="Contacts"></Section>
       </div>
     );
   }
